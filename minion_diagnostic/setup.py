33777356d85369641725bf9ff4dc183b7bd1f827
Sagar Sarkar <sarkars1@my.erau.edu>
2025-09-19 14:19:36 -0400

    Added the minion_diagnostic package.

---
+++ minion_diagnostic/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'minion_diagnostic'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sarkars1',
+    maintainer_email='sarkars1@my.erau.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+          'systeminfo_node = minion_diagnostic.systeminfo:main',
+          'system_health_monitor_node = minion_diagnostic.system_health_monitor:main',
+        ],
+    },
+)
